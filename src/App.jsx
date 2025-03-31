d89079ceb3068410c284e7caa191bad55629b4aa
Jorge Matos <jisapjisap@gmail.com>
2025-03-31 16:29:29 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/sections/Header";
+import Page from "./components/sections/Page";
+
+function App() {
+  return (
+    <Page>
+      <Header />
+    </Page>
+  )
+}
+
+export default App;
---
+++ src/components/sections/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div className="bg-gradient-to-bottom py-10">
+      Header
+    </div>
+  )
+}
+
+export default Header
