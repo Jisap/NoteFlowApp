61224dca65aaf36ed1cbd9dc4686ad40e8d3ab84
Jorge Matos <jisapjisap@gmail.com>
2025-03-31 18:01:45 +0200

    Update button styles in Navigation component for improved accessibility and consistency

--- src/components/sections/Navigation.jsx
+++ src/components/sections/Navigation.jsx
@@ -26,12 +26,12 @@ const Navigation = () => {
 
       <div className="flex items-center gap-x-3">
         <button className="border-primary-50 transition-properties hover:bg-primary-50 hover:text-primary-1300
-         box-border cursor-pointer rounded-full border-2 py-3.5 px-8 font-normal">
+         box-border cursor-pointer rounded-full border-2 py-3.5 px-8 font-normal text-lg/8">
           Login
         </button>
         <button className="bg-primary-500 border-primary-500 text-primary-1300 hover:border-primary-50
         hover:bg-primary-50 transition-properties cursor-pointer rounded-full border-2 px-8 py-3.5 font-normal primary-glow
-        hover:primary-glow-hover">
+        hover:primary-glow-hover text-lg/8">
           Get Started
         </button>
       </div>
