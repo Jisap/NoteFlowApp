a809e7a9183c356164bb22b88b1bd83a0985aff7
Jorge Matos <jisapjisap@gmail.com>
2025-04-01 13:39:32 +0200

    Add Main and Logos components, update App structure for improved layout

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -2,9 +2,9 @@
 
 const Header = ({ children }) => {
   return (
-    <div className="bg-gradient-to-bottom py-10">
+    <header className="bg-gradient-to-bottom py-10">
       { children }
-    </div>
+    </header>
   )
 }
 
---
+++ src/components/sections/Logos.jsx
@@ -0,0 +1,23 @@
+
+
+const Logos = () => {
+  return (
+    <section className="">
+      <div>
+        <p>
+          Organizations powered by <span>NoteFlow</span>{" "}
+        </p>
+        <div>
+          <ul>
+
+          </ul>
+          <ul>
+            
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Logos
