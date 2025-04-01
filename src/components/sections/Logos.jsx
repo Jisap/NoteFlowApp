bdba49ae0a3a0f2844581173023901fa66c20688
Jorge Matos <jisapjisap@gmail.com>
2025-04-01 13:56:29 +0200

    Enhance Logos component layout and styles for improved presentation and user engagement

--- src/components/sections/Logos.jsx
+++ src/components/sections/Logos.jsx
@@ -1,18 +1,39 @@
+import { logos } from "../../utils/content"
 
 
 const Logos = () => {
   return (
-    <section className="">
-      <div>
-        <p>
+    <section className="bg-gradient-to-top">
+      <div className="m-auto max-w-[90rem] px-24 py-28">
+        <p className="text-primary-50 m-auto mb-20">
           Organizations powered by <span>NoteFlow</span>{" "}
         </p>
         <div>
           <ul>
-
+            {logos.slice(0,4).map((logo) => {
+              return (
+                <li key={logo.id}>
+                  <img 
+                    src={logo.src} 
+                    alt={logo.alt} 
+                    className="" 
+                  />
+                </li>
+              )
+            })}
           </ul>
           <ul>
-            
+            {logos.slice(4).map((logo) => {
+              return (
+                <li key={logo.id}>
+                  <img
+                    src={logo.src}
+                    alt={logo.alt}
+                    className=""
+                  />
+                </li>
+              )
+            })}
           </ul>
         </div>
       </div>
