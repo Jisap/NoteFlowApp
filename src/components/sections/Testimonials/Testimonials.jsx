23c1eea97251e4fcddfe0340074f9b8da752aab1
Jorge Matos <jisapjisap@gmail.com>
2025-04-02 17:34:17 +0200

    Refactor Testimonials section with improved layout and styling for better user engagement

--- src/components/sections/Testimonials/Testimonials.jsx
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -7,28 +7,36 @@ const Testimonials = () => {
   const [expanded, setExpanded] = useState(true);
 
   return (
-    <section>
-      <div>
-        <p>Testimonials</p>
-        <h2>
-          What out AI-powered <br/>
-          notetaker have to say
-        </h2>
-        <p>
-          NoteFlow has helped 1000&apos;s supercharge their productivity with cutting edge AI note taking tools.
-        </p>
-      </div>
+    <section className="bg-gradient-to-top">
+      <div className="m-auto flex max-w-[90rem] flex-col items-center px-24 py-32">
+        <div className="mb-20 flex max-w-[51.625rem] flex-col items-center gap-y-6">
+          <p className="text-primary-1300 bg-primary-500 primary-glow w-min rounded-full px-4 py-2 text-base/8">
+            Testimonials
+          </p>
+          <h2 className="text-primary-50 text-center text-6xl/18 font-semibold tracking-tighter">
+            What out AI-powered <br />
+            notetaker have to say
+          </h2>
+          <p className="text-primary-100 px-28 text-center text-xl/loose font-light">
+            NoteFlow has helped 1000&apos;s supercharge their productivity with cutting edge AI note taking tools.
+          </p>
+        </div>
 
-      <div>
-        <TestimonialsList
-          textimonials={test.slice(0, expanded ? 5 : 2)}
-        />
-        <TestimonialsList
-          textimonials={test.slice(5, expanded ? 10 : 7)}
-        />
-        <TestimonialsList
-          textimonials={test.slice(0, expanded ? 15 : 12)}
-        />
+        <div>
+          <TestimonialsList
+            textimonials={test.slice(0, expanded ? 5 : 2)}
+          />
+          <TestimonialsList
+            textimonials={test.slice(5, expanded ? 10 : 7)}
+          />
+          <TestimonialsList
+            textimonials={test.slice(0, expanded ? 15 : 12)}
+          />
+        </div>
+
+        <button>
+          {expanded ? "Show less" : "Show more"}
+        </button>
       </div>
     </section>
   )
