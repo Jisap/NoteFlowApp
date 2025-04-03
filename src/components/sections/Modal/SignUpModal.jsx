e3ba9633cdb3d3b217ad275b4d5f5a4195823853
Jorge Matos <jisapjisap@gmail.com>
2025-04-03 18:13:40 +0200

    Enhance SignUpModal component with form submission handling and improved styling

--- src/components/sections/Modal/SignUpModal.jsx
+++ src/components/sections/Modal/SignUpModal.jsx
@@ -21,45 +21,75 @@ const SignUpModal = () => {
         [e.target.name]: e.target.value
       }
     })
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(checked){
+      console.log(input)
+      setInput(initialState)
+      // TODO: close modal
+      // TODO: submit data to database
+    }
   }
 
   return (
-    <section>
-      <div>
-        <h4>Lets Get You signedUp</h4>
-        <p>No charges, no fees. Get note taking in minutes!</p>
+    <section className="grid grid-cols-2 max-w-3xl ">
+      <div className="bg-primary-1300 flex flex-col justify-center gap-y-4b noise bg-repeat p-10 text-center">
+        <h4 className="text-primary-50 text-4xl/12 font-bold tracking-tight">
+          Lets Get You signedUp
+        </h4>
+        <p className="text-primary-100 text-lg/8">
+          No charges, no fees. Get note taking in minutes!
+        </p>
       </div>
 
-      <div>
-        <button>
-          <Close />
+      <div className="bg-primary-1500 flex flex-col justify-between gap-y-24 noise bg-repeat p-10">
+        <button className="border-primary-75 hover:bg-primary-75 group transition-properties ml-auto w-fit cursor-pointer rounded-2xl border-2 p-3">
+          <Close 
+            className="stroke-primary-75 group-hover:stroke-primary-1300 transition-properties"
+            width={2}
+          />
         </button>
-        <div>
-          <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            placeholder="Enter your email"
-            value={input.email}
-            onChange={handleInputs}
-          />
 
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            value={input.password}
-            onChange={handleInputs}
-          />
+        <div className="text-primary-50 flex flex-col gap-y-6 text-lg/8 font-semibold tracking-tight">
+          <label>
+            Email
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              value={input.email}
+              onChange={handleInputs}
+              className="bg-primary-75 placeholder-primary-1500 text-primary-1300 mt-2 block w-full rounded-full px-8 py-4 font-normal 
+              placeholder:text-base placeholder:font-light placeholder:opacity-20"
+            />
+          </label>
+
+          <label>
+            Password
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              value={input.password}
+              onChange={handleInputs}
+              className="bg-primary-75 placeholder-primary-1500 text-primary-1300 mt-2 block w-full rounded-full px-8 py-4 font-normal 
+              placeholder:text-base placeholder:font-light placeholder:opacity-20"
+            />
+          </label>
         </div>
 
         <div>
-          <div>
+          <div onClick={() => setChecked((prev) => !prev)}>
             <button>
               <Checkmark />
             </button>
+            <p>I agree to all terms</p>
           </div>
+          <button onClick={ handleSubmit }>
+            Get Started
+          </button>
         </div>
       </div>
     </section>
