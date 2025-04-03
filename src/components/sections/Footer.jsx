c1c5113a4f5eaf356f3ebb2fe77af3617a232a36
Jorge Matos <jisapjisap@gmail.com>
2025-04-03 16:08:10 +0200

    Enhance Footer component with improved styling and layout for better user experience

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -6,23 +6,36 @@ import Logo from "../icons/Logo"
 
 const Footer = () => {
   return (
-    <footer className="">
-      <div>
-        <div>
-          <a href="#">
-            <Logo />
+    <footer className="bg-gradient-to-bottom">
+      <div className="m-auto flex max-w-[90rem] justify-between px-24 py-32">
+        <div className="">
+          <a href="#" className="flex items-center gap-x-3">
+            <Logo 
+              className="stroke-primary-500" 
+              width={4.5} 
+              alt="NoteFlow logo icon" 
+            />
+            <p className="text-primary-50 text-xl font-bold tracking-tight">
+              NoteFlow
+            </p>
           </a>
-          <p>NoteFlow</p>
         </div>
 
-        <div>
+        <div className="grid grid-cols-[repeat(4,max-content)] gap-x-24">
           {footerCols.map((col) => (
             <div key={col.id}>
-              <p>{col.category}</p>
-              <ul>
+              <p className="text-primary-50 mb-8 text-xl/loose font-semibold">
+                {col.category}
+              </p>
+              <ul className="flex flex-col gap-y-4">
                 {col.links.map((link, i) => (
-                  <li key={i}>
-                      <a href="#">{link}</a>
+                  <li 
+                    key={i}
+                    className="cursor-pointer"  
+                  >
+                      <a href="#" className="text-primary-50 hover:text-primary-500 transition-properties text-lg/8 font-light">
+                        {link}
+                      </a>
                   </li>
                 ))}
               </ul>
