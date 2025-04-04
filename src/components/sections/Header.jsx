09da0108d5716ddef30909d470b883e1cae30a22
Jorge Matos <jisapjisap@gmail.com>
2025-04-04 16:40:15 +0200

    Enhance responsive design of Header and Navigation components with improved styling for better user experience on smaller screens

--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -2,7 +2,7 @@
 
 const Header = ({ children }) => {
   return (
-    <header className="bg-gradient-to-bottom py-10">
+    <header className="bg-gradient-to-bottom py-10 max-xl:py-8">
       { children }
     </header>
   )
--- src/components/sections/Navigation.jsx
+++ src/components/sections/Navigation.jsx
@@ -8,7 +8,7 @@ const Navigation = () => {
 
 
   return (
-    <nav className="text-primary-50 m-auto flex justify-between max-w-[90rem] px-24 text-lg/8 font-light">
+    <nav className="text-primary-50 m-auto flex justify-between max-w-[90rem] px-24 text-lg/8 font-light max-xl:px-16 max-xl:text-base/loose">
       <a href="/" className="flex items-center gap-x-3">
         <Logo 
           className="h-6"
@@ -19,7 +19,7 @@ const Navigation = () => {
         </p>
       </a>
 
-      <ul className="flex items-center gap-x-8">
+      <ul className="flex items-center gap-x-8 max-xl:gap-x-6">
         {navigationLinks.map((link) => (
           <li key={link.id}>
             <a href={link.href} className="hover:text-primary-500 transition-properties">
@@ -31,13 +31,13 @@ const Navigation = () => {
 
       <div className="flex items-center gap-x-3">
         <button className="border-primary-50 transition-properties hover:bg-primary-50 hover:text-primary-1300
-         box-border cursor-pointer rounded-full border-2 py-3.5 px-8 font-normal text-lg/8">
+         box-border cursor-pointer rounded-full border-2 py-3.5 px-8 font-normal text-lg/8 max-xl:px-6 max-xl:py-3 max-xl:text-base/loose">
           Login
         </button>
         <button 
           className="bg-primary-500 border-primary-500 text-primary-1300 hover:border-primary-50
         hover:bg-primary-50 transition-properties cursor-pointer rounded-full border-2 px-8 py-3.5 font-normal primary-glow
-        primary-glow-hover text-lg/8"
+        primary-glow-hover text-lg/8 max-xl:px-6 max-xl:py-3 max-xl:text-base/loose"
           onClick={() => setActiveModal("sign-up")}
         >
           Get Started
