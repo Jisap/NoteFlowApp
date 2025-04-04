4fac1b778aa3397fb6ed006dfbd71d286821b4de
Jorge Matos <jisapjisap@gmail.com>
2025-04-04 17:40:41 +0200

    Enhance Logos component with responsive design adjustments for improved user experience on smaller screens

--- src/components/sections/Logos.jsx
+++ src/components/sections/Logos.jsx
@@ -4,8 +4,8 @@ import { logos } from "../../utils/content"
 const Logos = () => {
   return (
     <section className="bg-gradient-to-top">
-      <div className="m-auto max-w-[90rem] px-24 py-28">
-        <p className="text-primary-50 m-auto mb-20 text-center text-xl/loose">
+      <div className="m-auto max-w-[90rem] px-24 py-28 max-xl:px-16 max-xl:py-24">
+        <p className="text-primary-50 m-auto mb-20 text-center text-xl/loose max-xl:text-lg/8">
           Organizations powered by <span className="font-bold tracking-tight">NoteFlow</span>{" "}
         </p>
         <div className="flex flex-col gap-y-16">
@@ -16,7 +16,7 @@ const Logos = () => {
                   <img 
                     src={logo.src} 
                     alt={logo.alt} 
-                    className="h-10" 
+                    className="h-10 max-xl:h-8" 
                   />
                 </li>
               )
@@ -29,7 +29,7 @@ const Logos = () => {
                   <img
                     src={logo.src}
                     alt={logo.alt}
-                    className="h-10"
+                    className="h-10 max-xl:h-8"
                   />
                 </li>
               )
