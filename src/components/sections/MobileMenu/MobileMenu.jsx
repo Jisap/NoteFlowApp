97f0c0534ce1db9d063ce21c021c4f2c1517cd0c
Jorge Matos <jisapjisap@gmail.com>
2025-04-05 13:21:34 +0200

    Refactor button styles in MobileMenu for consistency and improved readability

--- src/components/sections/MobileMenu/MobileMenu.jsx
+++ src/components/sections/MobileMenu/MobileMenu.jsx
@@ -37,12 +37,12 @@ const MobileMenu = () => {
 
         <div className="flex flex-col gap-y-3">
           <button className="border-primary-50 transition-properties test-primary-50 hover:bg-primary-50 hover:text-primary-1300 
-          box-border cursor-pointer rounded-full border-2 px-6 py-3"
+          box-border cursor-pointer rounded-full border-2 px-6 py-3 text-base/loose font-normal"
           >
             Login
           </button>
           <button className="bg-primary-500 border-primary-500 text-primary-1300 primary-glow hover:border-primary-50
-           hover:bg-primary-50 primary-glow-hover transition-properties cursor-pointer rounded-full border-2 px-6 py-3"
+           hover:bg-primary-50 primary-glow-hover transition-properties cursor-pointer rounded-full border-2 px-6 py-3 text-base/loose "
           >
             Get Started
           </button>
