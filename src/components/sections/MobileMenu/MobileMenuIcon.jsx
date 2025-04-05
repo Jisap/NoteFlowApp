224b023a088b4adb9c52311531efa94168904568
Jorge Matos <jisapjisap@gmail.com>
2025-04-05 11:58:20 +0200

    MobileMenuIcon

--- src/components/sections/MobileMenu/MobileMenuIcon.jsx
+++ src/components/sections/MobileMenu/MobileMenuIcon.jsx
@@ -3,16 +3,16 @@ import Menu from "../../icons/Menu"
 
 
 const MobileMenuIcon = () => {
-  
+
   const mobileMenuOpened = true
-  
+
   return (
     <motion.button
       animate={{ rotate: mobileMenuOpened ? 90 : 0 }}
-      transition={{ duration:0.2, ease: "easeInOut" }}
+      transition={{ duration: 0.2, ease: "easeInOut" }}
       className="justify-center items-center max-lg:flex hidden cursor-pointer"
     >
-      <Menu className="stroke-primary-50 h-7 w-7" width={2}/>
+      <Menu className="stroke-primary-50 h-7 w-7" width={2} />
     </motion.button>
   )
 }
