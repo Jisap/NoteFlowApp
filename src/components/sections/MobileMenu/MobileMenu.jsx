731c4979eadc5d559b609f45dba0019bafd42c56
Jorge Matos <jisapjisap@gmail.com>
2025-04-05 13:16:10 +0200

    Refactor MobileMenu component for improved structure and styling

--- src/components/sections/MobileMenu/MobileMenu.jsx
+++ src/components/sections/MobileMenu/MobileMenu.jsx
@@ -7,25 +7,45 @@ import { navigationLinks } from "../../../utils/content"
 
 const MobileMenu = () => {
   return (
-    <motion.div>
-      <motion.div>
+    <motion.div
+      className="bg-primary-1300/50 fixed top-0 bottom-0 left-0 right-0 z-50 flex justify-end px-6 py-6 pl-28 backdrop-blur-sm"
+    >
+      <motion.div
+        className="bg-primary-1400 flex basis-80 flex-col justify-between rounded-2xl noise bg-repeat px-6 py-8"
+      >
         <div>
-          <button>
-            <Close  width={2} />
+          <button className="border-primary-75 hover:bg-primary-75 group transition-properties mr-auto w-fit cursor-pointer rounded-2xl border-2 p-3">
+            <Close  
+              width={2} 
+              className="stroke-primary-75 group-hover:stroke-primary-1300 transition-properties h-4 w-4" 
+            />
           </button>
 
-          <ul>
+          <ul className="mt-16 flex flex-col gap-y-6">
             {navigationLinks.map((link) => (
               <li key={link.id}>
-                <a href={link.href}>{link.link}</a>
+                <a 
+                  href={link.href}
+                  className="text-primary-50 hover:text-primary-500 transition-properties text-lg/8"
+                >
+                  {link.link}
+                </a>
               </li>
             ))}
           </ul>
         </div>
 
-        <div>
-          <button>Login</button>
-          <button>Get Started</button>
+        <div className="flex flex-col gap-y-3">
+          <button className="border-primary-50 transition-properties test-primary-50 hover:bg-primary-50 hover:text-primary-1300 
+          box-border cursor-pointer rounded-full border-2 px-6 py-3"
+          >
+            Login
+          </button>
+          <button className="bg-primary-500 border-primary-500 text-primary-1300 primary-glow hover:border-primary-50
+           hover:bg-primary-50 primary-glow-hover transition-properties cursor-pointer rounded-full border-2 px-6 py-3"
+          >
+            Get Started
+          </button>
         </div>
       </motion.div>
     </motion.div>
