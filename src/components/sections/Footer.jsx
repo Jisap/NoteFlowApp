4ffc953e0a42d6358d890036710def9c7a6f61d4
Jorge Matos <jisapjisap@gmail.com>
2025-04-07 18:08:54 +0200

    Enhance Footer component for improved mobile responsiveness

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -7,24 +7,24 @@ import Logo from "../icons/Logo"
 const Footer = () => {
   return (
     <footer className="bg-gradient-to-bottom">
-      <div className="m-auto flex max-w-[90rem] justify-between px-24 py-32 max-xl:px-16 max-xl:py-24 max-lg:grid max-lg:gap-y-18 max-lg:px-8">
+      <div className="m-auto flex max-w-[90rem] justify-between px-24 py-32 max-xl:px-16 max-xl:py-24 max-lg:grid max-lg:gap-y-18 max-lg:px-8 max-md:px-6 max-sm:pb-16">
         <div className="">
-          <a href="#" className="flex items-center gap-x-3">
+          <a href="#" className="flex items-center gap-x-3 max-md:gap-x-2">
             <Logo 
-              className="stroke-primary-500" 
+              className="stroke-primary-500 max-md:h-5" 
               width={4.5} 
               alt="NoteFlow logo icon" 
             />
-            <p className="text-primary-50 text-xl font-bold tracking-tight">
+            <p className="text-primary-50 text-xl font-bold tracking-tight max-md:text-lg/8">
               NoteFlow
             </p>
           </a>
         </div>
 
-        <div className="grid grid-cols-[repeat(4,max-content)] gap-x-24 max-xl:gap-x-18 max-lg:gap-x-24">
+        <div className="grid grid-cols-[repeat(4,max-content)] gap-x-24 max-xl:gap-x-18 max-lg:gap-x-24 max-md:grid-cols-2 max-md:gap-y-14">
           {footerCols.map((col) => (
             <div key={col.id}>
-              <p className="text-primary-50 mb-8 text-xl/loose font-semibold">
+              <p className="text-primary-50 mb-8 text-xl/loose font-semibold max-md:text-lg/8 max-md:tracking-tight">
                 {col.category}
               </p>
               <ul className="flex flex-col gap-y-4">
